a3b512298d3ded474cdffc18e684ba379c013e76
chenjing <chenjing@192.168.135.102>
2018-06-30 08:48:04 +0800

    2222

--- src/main/java/com/example/demo/GithubTestApplication.java
+++ src/main/java/com/example/demo/GithubTestApplication.java
@@ -8,7 +8,7 @@ public class GithubTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubTestApplication.class, args);
-		
+		System.out.println("111");
 		
 	}
 }
